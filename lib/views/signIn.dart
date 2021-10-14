3b73686dfdb4bc2002014e23a431a790a7403198
HieuHD <hieu987020@gmail.com>
2021-10-14 15:10:29 +0700

    first commit

---
+++ lib/views/signIn.dart
@@ -0,0 +1,17 @@
+import 'package:chat_app/widgets/widget.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Center(),
+    );
+  }
+}
